b88438038d7363a4efa0e93e92b6c117a7811175
Shizen39 <mazzagiorgio96@gmail.com>
2019-03-08 10:11:40 +0100

    gitignore

--- program_chromosome.py
+++ program_chromosome.py
@@ -1,19 +1,34 @@
 def get_action(observation, states):
-	if observation[2] > states[2][0]:
-    		if observation[2] > states[2][1]:
-			if observation[3] > states[3][0]:
-				if observation[2] <= states[2][0]:
+	if observation[2] <= states[2][2]:
+		if observation[2] > states[2][1]:
+			if observation[1] > states[1][1]:
+				if observation[2] <= states[2][2]:
 					action = 1
 
+			else:
+				if observation[1] > states[1][1]:
+					action = 0
+
+				else:
+					action = 0
+
 		else:
-			if observation[3] > states[3][0]:
-				if observation[2] <= states[2][0]:
+			if observation[1] <= states[1][1]:
+				if observation[2] <= states[2][2]:
+					action = 0
+
+				else:
 					action = 1
 
 	else:
-		if observation[3] > states[3][0]:
-			if observation[2] <= states[2][0]:
-				if observation[3] <= states[3][2]:
+		if observation[1] > states[1][1]:
+			if observation[2] <= states[2][2]:
+				if observation[1] <= states[1][1]:
+					action = 0
+
+		else:
+			if observation[2] <= states[2][2]:
+				if observation[1] <= states[1][1]:
 					action = 0
 
 	return action
